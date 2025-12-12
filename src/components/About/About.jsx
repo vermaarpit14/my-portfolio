447570a89a9a4fa5734a4c0675bd2c547b055e60
vermaarpit14 <vermaarpit253@gmail.com>
2025-12-12 13:04:01 +0530

    ..

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -57,7 +57,7 @@ const About = () => {
 
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1_pLl2wjYVCU-wnqXIhjhYr0YC0SJXvwv/view?usp=sharing"
+            href="https://drive.google.com/file/d/1wG31372f4gKcTnUwPllX0z9vMWVlfJMQ/view?usp=drivesdk"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
